5717b3fbcc5747fb604c0977ce068f85c0577287
Jorghee <jorgeehuarsaya@gmail.com>
2024-05-29 18:27:54 -0500

    Agregar vistas para la gestión de estudiantes, cursos y notas
    - Añadida la vista home para renderizar la página principal.
    - Añadida la vista create_student para crear nuevos estudiantes.
    - Añadida la vista create_course para crear nuevos cursos.
    - Añadida la vista create_student_grade para registrar las notas de los
    estudiantes.

--- university/grades/views.py
+++ university/grades/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from .models import Student, Course, StudentGrade
+from .forms import StudentForm, CourseForm, StudentGradeForm
 
-# Create your views here.
+def home(request):
+  return render(request, 'grades/home.html')
+
+def create_student(request):
+  if request.method == 'POST':
+    form = StudentForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+  else:
+    form = StudentForm()
+  return render(request, 'grades/create_student.html', {'form': form})
+
+def create_course(request):
+  if request.method == 'POST':
+    form = CourseForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+  else:
+    form = CourseForm()
+  return render(request, 'grades/create_course.html', {'form': form})
+
+def create_student_grade(request):
+  if request.method == 'POST':
+    form = StudentGradeForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+  else:
+    form = StudentGradeForm()
+  return render(request, 'grades/create_student_grade.html', {'form': form})
