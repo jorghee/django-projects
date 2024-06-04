91a47438dc59156b31a0ff5b0b8d023eebdbb959
Jorghee <jorgeehuarsaya@gmail.com>
2024-06-04 15:25:55 -0500

    Configuración del directorio static/ y assets/
    Se estableció la variable STATICFILES_DIRS para que Django pueda
    encontrar los recursos necesarios, como .css, .js, .jpg, .svg entre
    otras.

--- turismo_Peru/turismo_Peru/settings.py
+++ turismo_Peru/turismo_Peru/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Dos setup importantes
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
