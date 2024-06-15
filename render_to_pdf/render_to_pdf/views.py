6a7e5b0a3d11d025de8827ea4eb171e4fde2d8f3
Jorghee <jorgeehuarsaya@gmail.com>
2024-06-15 16:24:43 -0500

    Imprimiendo datos en el pdf

--- render_to_pdf/render_to_pdf/views.py
+++ render_to_pdf/render_to_pdf/views.py
@@ -4,9 +4,10 @@ from . import renderers
 
 def pdf_view(request, *args, **kwargs):
   data = {
+    'pdf_title': "Factura Rovistar",
     'today': datetime.date.today(), 
     'amount': 39.99,
-    'customer_name': 'Cooper Mann',
+    'customer_name': 'Jorge Luis Mamani Huarsaya',
     'invoice_number': 1233434,
   }
   return renderers.render_to_pdf('pdf/invoice.html', data)
